56f29e1fa679eac8637257fcce4af474ea9811a3
ParkerCatz <tksm.webdesign09@gmail.com>
2021-07-15 23:54:53 +0900

    feat: cors and proxy

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const PORT = 3001
 const fs = require('fs')
 const path = require('path')
 const pathToFile = path.resolve('./data.json')
+const cors = require('cors')
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 
 const getResources = () => JSON.parse(fs.readFileSync(pathToFile))
 
